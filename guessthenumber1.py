486cd0f1296d2c8b6b8fa04fd31638070add6c0d
Alfi <emceealfi@gmail.com>
2021-12-10 13:01:54 +0800

    This is the the first commit

---
+++ guessthenumber1.py
@@ -0,0 +1,18 @@
+import random #packages/libraries/modules
+n = random.randint(1, 99)
+
+guess = int(input("Enter a number from 1 to 99: "))
+
+while (guess != n):
+    if(guess > n):
+        print("Guessing number is big! ")
+        guess=int(input("Enter a smaller Number : "))
+        continue
+    elif(guess < n):
+        print("Guessing number is small !")
+        guess=int(input("Enter a higher Number : "))
+        continue
+if(guess== n):
+    print("You guessed the correct the number!")
+print("Random number is: " )
+print(guess)
